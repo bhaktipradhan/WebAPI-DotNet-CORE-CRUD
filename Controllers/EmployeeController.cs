2fac6e4dadb7a9c4a886cd741cfc78843b9eea0d
bhaktipradhan <bhaktipradhan07@gmail.com>
2024-06-18 09:39:40 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using WebAPIV8.Data;
+using WebAPIV8.Models;
+using WebAPIV8.Models.Entities;
+
+namespace WebAPIV8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployee()
+        {
+            var employee = dbContext.Employees.ToList();
+            return Ok(employee);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id) 
+        { 
+            var employee = dbContext.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeModel viewModel)
+        {
+            var employee = new Employee()
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed
+            };
+            dbContext.Employees.Add(employee);
+            dbContext.SaveChanges();
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult EditEmloyee(Guid id, UpdateEmployeeModel updateTbl)
+        {
+            var employee = dbContext.Employees.Find(id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+            employee.Name = updateTbl.Name;
+            employee.Email = updateTbl.Email;
+            employee.Phone = updateTbl.Phone;
+            employee.Subscribed = updateTbl.Subscribed;
+            dbContext.SaveChanges();
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employee = dbContext.Employees.Find(id);
+            if(employee is null)
+            {
+                return NotFound();
+            }
+            dbContext.Employees.Remove(employee);
+            dbContext.SaveChanges();
+            return Ok(employee);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIV8.Models.Entities;
+
+namespace WebAPIV8.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
